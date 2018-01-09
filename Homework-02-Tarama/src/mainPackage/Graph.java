b7a6c64f1bd29798182cd9ac57cf94e0abe47315
SystemRoott <System_Root@outlook.com.tr>
2018-01-09 23:27:30 +0300

    Update

--- Homework-02-Tarama/src/mainPackage/Graph.java
+++ Homework-02-Tarama/src/mainPackage/Graph.java
@@ -1,55 +1,62 @@
 package mainPackage;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Graph {
-	String[] neighbors;
+	LinkedList<String> neighbors;
 	int[][] relationships;
-	String[] weights;
-	public Graph (String[] neighbors, String[] weights)
-	{
+	LinkedList<String> weights;
+
+	public Graph(LinkedList<String> neighbors) {
 		try {
 			this.neighbors = neighbors;
-			this.weights = weights;
-			
-			this.relationships = new int[this.neighbors.length][this.neighbors.length];
-			for(int i = 0; i < this.relationships.length; i++)
-			{
+			this.weights = new LinkedList<String>();
+			this.weights.addLast("0");
+			this.relationships = new int[this.neighbors.size()][this.neighbors.size()];
+			for (int i = 0; i < this.relationships.length; i++) {
 				this.relationships[i][i] = 0;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public  void addRelation(int from, int to, int index)
-	{
+
+	public void addRelation(int from, int to, int index) {
 		try {
+			// System.out.println("From: " + from + " To: " + to + " Index: " + index);
 			this.relationships[from][to] = index;
 			this.relationships[to][from] = index;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	public void print() {
 		try {
+			String[][] data = new String[this.neighbors.size()][this.neighbors.size()];
 			System.out.print('\t');
-			for(int i = 0; i < this.neighbors.length; i++)
+			String leftAlignFormat = "%20s";
+			String leftAlignFormat_ = "%3s";
+			for(int i = 1; i < this.neighbors.size(); i++)
 			{
-				System.out.print(this.neighbors[i] + "\t");
+				data[0][i] = this.neighbors.get(i);
 			}
-			System.out.println("");
-			for(int i = 0; i < this.neighbors.length; i++)
+			for(int i = 1; i < this.neighbors.size(); i++)
 			{
-				System.out.print(this.neighbors[i] + "\t");
-				for(int j = 0; j < this.neighbors.length ; j++)
+				data[i][0] = this.neighbors.get(i);
+				for(int j = 1; j < this.neighbors.size() ; j++)
 				{
-					System.out.print(this.weights[this.relationships[i][j]]  + "\t" );
+					data[i][j] = this.weights.get(this.relationships[i][j]);
 				}
-				System.out.println("");
 			}
-			
+			for(int i = 0; i < data.length; i++)
+			{
+				System.out.println(Arrays.toString(data[i]));
+			}
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
--- Homework-02-Tarama/src/mainPackage/mainClass.java
+++ Homework-02-Tarama/src/mainPackage/mainClass.java
@@ -5,70 +5,74 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.LinkedList;
 
+import com.sun.glass.ui.Clipboard;
+
 public class mainClass {
 
-	
-	
-	private static Graph getGraphFromFile(File file)
-	{
+	private void addRelationsAndWeights(String data, Graph graph, int index) {
+		String[] fragmentedData = data.split("-");
+		graph.weights.addLast(fragmentedData[0]);
+
+		LinkedList<Integer> Ids = new LinkedList<Integer>();
+		for (int i = 1; i < fragmentedData.length; i++) {
+
+			if (fragmentedData[i].equalsIgnoreCase("1")) {
+				Ids.addLast(i);
+			}
+		}
+
+		// System.out.println(index);
+		if (Ids.size() % 2 == 0) {
+			for (int i = 0; i < Ids.size(); i += 2) {
+				 //System.out.println(Ids.get(i) + " " + Ids.get(i+1) + " " + index);
+				graph.addRelation(Ids.get(i), Ids.get(i + 1), index);
+			}
+		} else if (Ids.size() % 2 == 1) {
+			while (Ids.size() != 0) {
+				for (int i = 1; i < Ids.size() && Ids.size() != 1; i++) {
+					// System.out.println(Ids.get(0) + " " + Ids.get(i) + " " + index);
+					graph.addRelation(Ids.get(0), Ids.get(i), index);
+				}
+				Ids.removeFirst();
+			}
+		}
+
+	}
+
+	private static Graph getGraphFromFile(File file) {
 		Graph result = null;
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line = br.readLine();
 			String[] tmp = line.split("-");
 			tmp[0] = "";
-			String[] neighbors = tmp.clone();
-		
+			LinkedList<String> neighbors = new LinkedList<String>();
+			neighbors.addLast("");
+			for (int i = 1; i < tmp.length; i++) {
+				neighbors.addLast(tmp[i]);
+			}
+
+			result = new Graph(neighbors);
+			int index = 1;
+			mainClass main = new mainClass();
 			for (String line1; (line1 = br.readLine()) != null;) {
-				line += line1;
-				line += "&";
+				main.addRelationsAndWeights(line1, result, index);
+				index++;
 			}
-			tmp = line.split("&");
-			
-			String[] weights = new String[tmp.length];
 
-			LinkedList<LinkedList> tmp3 = new LinkedList<LinkedList>();
-			for(int i = 1; i < tmp.length; i++)
-			{
-
-				LinkedList<Integer> tmpIndex = new LinkedList<Integer>();
-				String[] tmp2 = tmp[i].split("-");
-				weights[i] = tmp2[0];
-				for(int j = 1; j < tmp2.length; j++)
-				{
-					if(tmp2[j] == "1")
-					{
-						tmpIndex.add(j);
-					}
-				}
-
-				tmp3.add(tmpIndex);
-				
-				
-			}
-			result = new Graph(neighbors, weights);
-			
-			for(int i = 0; i < tmp3.size(); i++)
-			{
-				LinkedList<Integer> tmp4 = tmp3.get(i);
-				for(int k = 0; k < tmp4.size(); k+=2)
-				{
-					result.addRelation(k, k+1, i);
-				}
-			}
-			
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		File file = new File("C:\\graphdata.txt");
 		Graph test = getGraphFromFile(file);
+		
 		test.print();
+		
 	}
 
 }
