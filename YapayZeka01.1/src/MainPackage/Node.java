b4f1b3d22720f37fa1c38e0e3c88962a904c2017
rootssystem <rootssystem@parrot>
2018-10-20 14:21:18 +0300

    Bug

--- YapayZeka01.1/src/MainPackage/Node.java
+++ YapayZeka01.1/src/MainPackage/Node.java
@@ -6,72 +6,80 @@ public class Node {
 	int nodeId;
 	Node nodes[];
 	int nodeLevel;
-
 	public Node() {
 
 	}
 
-	public Node(int NodeCount, int nodeId) {
-		nodes = new Node[NodeCount];
-		this.nodeId = nodeId;
+	public Node(int NodeCount, int nodeId) { //initialize node
+		nodes = new Node[NodeCount]; //set connection list
+		this.nodeId = nodeId; //set nodeId
 	}
 
-	public Node findNeighbors(int[][] matrix, int goalId, LinkedList<Integer> prevs) {
-		Node result = this;
-		prevs.add(this.nodeId);
-		int ConnectionCount = 0;
-		if (this.nodeId == goalId) {
+	public Node CreateTree(int[][] matrix, int goalId, LinkedList<Integer> prevs) {
+		Node result = this; //set result to this object
+		prevs.add(this.nodeId); // add tick to visited
+		int ConnectionCount = 0; // Node connection count except visited
+		if (this.nodeId == goalId) { //if this node is goalNode, return this node;
 			result = this;
 		} else {
-			for (int i = 0; i < nodes.length; i++) {
+			for (int i = 0; i < nodes.length; i++) { //search matrix to find a connection 
 				if (matrix[nodeId][i] != 0) {
-					if (prevs.contains(i)) {
-
+					if (prevs.contains(i)) { // if i is visited, pass
 						continue;
 					}
-					Node tmp = new Node(nodes.length, i);
-					tmp = tmp.findNeighbors(matrix, goalId, prevs);
-					if (tmp == null) {
+					Node tmp = new Node(nodes.length, i); // else create a node of connection
+					tmp.nodeLevel = this.nodeLevel + 1;//set node level
+					tmp = tmp.CreateTree(matrix, goalId, prevs);//find a connection of new node with recursive call
+					if (tmp == null) {//if return is null, pass
 						continue;
 					} else {
-						tmp.nodeLevel = this.nodeLevel + 1;
-						nodes[ConnectionCount] = tmp;
-						ConnectionCount++;
-						this.removeData(prevs, i);
+						nodes[ConnectionCount] = tmp; //else set tmp to node list
+						ConnectionCount++;//increase connection count
+						this.removeData(prevs, i);//remove connection from visited list
 					}
 
 				}
 			}
 
 		}
-		return result;
+		return result; //return result
 	}
 
 	private LinkedList<Integer> removeData(LinkedList<Integer> list, int data) {
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i) == data) {
+		for (int i = 0; i < list.size(); i++) {//search list to find incoming data
+			if (list.get(i) == data) { //if its found, remove
 				list.remove(i);
 			}
 		}
 		return list;
 	}
 
-	public void print(char edges[], int level, LinkedList<String> lines) {
+	public void print(char edges[],  LinkedList<String> lines) {
 
-		if (lines.size() < level + 1) {
+		if (lines.size() <= this.nodeLevel) { // if level doesn't exist in line list, create
 			lines.addLast("");
 		}
-		String tmp = lines.get(level);
-		for (int j = 0; j < this.nodes.length; j++) {
-			if (nodes[j] != null) {
+		String tmp = lines.get(this.nodeLevel); //get line of level
+		boolean oneNode = true; 
+		for (int j = 0; j < this.nodes.length; j++) {//search node connections
+			if (nodes[j] != null) { // if exist, print space and set oneNode to false because its not.
+				tmp += "  ";
+				oneNode = false;
+			}
+		}
+		if(oneNode)//if its oneNode
+		{
+			String topLine = lines.get(this.nodeLevel-1);//get top line
+			for(int i = 0; i < topLine.length()-2;i++) { //print space 'till topLine length-2
+
 				tmp += " ";
 			}
 		}
-		lines.set(level, tmp += edges[this.nodeId] + " ");
+		lines.set(this.nodeLevel, tmp += edges[this.nodeId]);//add node name to end of line and replace from list
 
-		for (int i = 0; i < this.nodes.length; i++) {
-			if (nodes[i] != null) {
-				nodes[i].print(edges, this.nodeLevel + 1, lines);
+		for (int i = 0; i < this.nodes.length; i++) {//search node connections
+			if (nodes[i] != null) {//if its found, call 'print' for print itself and his connections
+				nodes[i].print(edges, lines);
 			}
 		}
 
